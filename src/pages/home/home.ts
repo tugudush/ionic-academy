e24788ebb2b0b96448fb02fd19c63ff980a8e433
tugudush <jerome2kph@gmail.com>
2017-09-12 10:39:52 +0800

    updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,25 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage {  
   constructor(public navCtrl: NavController) {
 
   }
 
+  page_title = "Home";
+  greetings = "Hello World";
+
+  a = "4";
+  b = "6";
+  result = 0;
+
+  getText() {
+    return this.greetings;
+  }
+
+  calculate() {
+    this.result = parseInt(this.a) + parseInt(this.b);
+  }
+
+  myArray = ['this', 'is', 'my', 'first', 'ngFor'];
 }
